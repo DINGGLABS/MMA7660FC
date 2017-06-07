7dd6ffb05cea09bca40119506e0ef4c4226703f9
Jascha Haldemann <jh@oxon.ch>
2017-06-07 13:26:52 +0200

    unified all headers

--- MMA7660FC.cpp
+++ MMA7660FC.cpp
@@ -1,130 +1,191 @@
-/**-----------------------------------------------------------------------------
- * \file    MMA7660FC.h
- * \author  jh
- * \date    xx.11.2016
- * @{
- -----------------------------------------------------------------------------*/
+/*******************************************************************************
+* \file    MMA7660FC.cpp
+********************************************************************************
+* \author  Jascha Haldemann jh@oxon.ch
+* \date    01.11.2016
+* \version 1.0
+*******************************************************************************/
 
- /* Includes --------------------------------------------------- */
+/* ================================= Imports ================================ */
 #include "MMA7660FC.h"
 
-/* Public ----------------------------------------------------- */
-void MMA7660FC::begin(const uint32_t clockSpeed)
-{
-  AccelWire_.begin();
-  AccelWire_.setClock(clockSpeed);
+/* ======================= Module constant declaration ====================== */
 
-  setMode(STAND_BY);
-  setSampleRate(AUTO_SLEEP_120);
-  setMode(ACTIVE);
-}
+/* ======================== Module macro declaration ======================== */
 
-void MMA7660FC::end()
-{
-  setMode(STAND_BY);
-  AccelWire_.end();
-}
+/* ====================== Module class instantiations ======================= */
 
-void MMA7660FC::setMode(mode modeNr)
-{
-  writeRegister(REG_MODE, modeNr);
-}
-
-void MMA7660FC::setSampleRate(sample_rate rateNr)
-{
-  writeRegister(REG_SR, rateNr);
-}
-
-void MMA7660FC::getAccelerationVector(int8_t vector[3])
-{
-  bool alert;
-  do
+/* ======================== Public member Functions ========================= */
+/** -------------------------------------------------------------------------
+  * \fn     begin
+  * \brief  inits the accelerometer
+  --------------------------------------------------------------------------- */
+  void MMA7660FC::begin(const uint32_t clockSpeed)
   {
-    alert = false;
-    AccelWire_.beginTransmission(I2C_ADDR);
-    AccelWire_.write(REG_X);
-    AccelWire_.endTransmission();
-    AccelWire_.requestFrom(I2C_ADDR, uint8_t(3));
-    for (uint8_t i = 0; i < 3; i++)  // x, y, z
-    {
-      /* get register */
-      vector[i] = AccelWire_.read();
-      /* check the alert-bit */
-      if (vector[i] & (1 << 6)) alert = true;
-      else vector[i] = int8_t(vector[i] << 2) >> 2;   // calculate the signed 6-bit acceleration
-    }
-  } while (alert);  // repeat the whole crap if an alert bit is set!
-}
+    AccelWire_.begin();
+    AccelWire_.setClock(clockSpeed);
 
-void MMA7660FC::getAccelerationVector(float vector[3], bool convert)
-{
-  int8_t tempVector[3];
-  getAccelerationVector(tempVector);
-  vector[0] = tempVector[0];
-  vector[1] = tempVector[1];
-  vector[2] = tempVector[2];
-
-  if (convert)
-  {
-    vector[0] /= G_DIVISOR;
-    vector[1] /= G_DIVISOR;
-    vector[2] /= G_DIVISOR;
+    setMode(STAND_BY);
+    setSampleRate(AUTO_SLEEP_120);
+    setMode(ACTIVE);
   }
 
-  // bool alert;
-  // do
-  // {
-  //   alert = false;
-  //   AccelWire_.beginTransmission(I2C_ADDR);
-  //   AccelWire_.write(REG_X);
-  //   AccelWire_.endTransmission();
-  //   AccelWire_.requestFrom(I2C_ADDR, uint8_t(3));
-  //   for (uint8_t i = 0; i < 3; i++)  // x, y, z
-  //   {
-  //     /* get register */
-  //     vector[i] = AccelWire_.read();
-  //     /* check the alert-bit */
-  //     if (vector[i] & (1 << 6)) alert = true;
-  //     else vector[i] = int8_t(vector[i] << 2) >> 2;   // calculate the signed 6-bit acceleration
+/** -------------------------------------------------------------------------
+  * \fn     end
+  * \brief  deinits the accelerometer
+  --------------------------------------------------------------------------- */
+  void MMA7660FC::end()
+  {
+    setMode(STAND_BY);
+    AccelWire_.end();
+  }
 
-  //     if (convert) vector[i] /= G_DIVISOR;
-  //   }
-  // } while (alert);  // repeat the whole crap if an alert bit is set!
-}
+/** -------------------------------------------------------------------------
+  * \fn     setMode
+  * \brief  sets the mode of the accelerometer
+  *
+  * \param  modeNr  mode number (see enumerator in the header file)
+  * \return None
+  --------------------------------------------------------------------------- */
+  void MMA7660FC::setMode(mode_t modeNr)
+  {
+    writeRegister(REG_MODE, modeNr);
+  }
 
-MMA7660FC::orientation MMA7660FC::getOrientation()
-{
-  orientation o = UNKNOWN;
-  int8_t vector[3] = {0};
-  getAccelerationVector(vector);
-  if      (vector[2] <= -ACCEL_THRESHOLD) o = DOWN;
-  else if (vector[2] >=  ACCEL_THRESHOLD) o = UP;
-  else if (vector[0] <= -ACCEL_THRESHOLD) o = HORIZONTALLY;
-  else if (vector[0] >=  ACCEL_THRESHOLD) o = HORIZONTALLY;
-  else if (vector[1] <= -ACCEL_THRESHOLD) o = VERTICALLY;
-  else if (vector[1] >=  ACCEL_THRESHOLD) o = VERTICALLY;
-  return o;
-}
+/** -------------------------------------------------------------------------
+  * \fn     setSampleRate
+  * \brief  sets the sample rate of the accelerometer
+  *
+  * \param  rateNr  rate number (see enumerator in the header file)
+  * \return None
+  --------------------------------------------------------------------------- */
+  void MMA7660FC::setSampleRate(sampleRate_t rateNr)
+  {
+    writeRegister(REG_SR, rateNr);
+  }
 
-/* Private ---------------------------------------------------- */
-uint8_t MMA7660FC::readRegister(uint8_t address)
-{
-  AccelWire_.beginTransmission(I2C_ADDR);
-  AccelWire_.write(address);
-  AccelWire_.endTransmission();
+/** -------------------------------------------------------------------------
+  * \fn     getAccelerationVector
+  * \brief  gets the acceleration vector (x, y, z)
+  *
+  * \param  vector  acceleration array pointer (int8)
+  * \return None
+  --------------------------------------------------------------------------- */
+  void MMA7660FC::getAccelerationVector(int8_t vector[3])
+  {
+    bool alert;
+    do
+    {
+      alert = false;
+      AccelWire_.beginTransmission(I2C_ADDR);
+      AccelWire_.write(REG_X);
+      AccelWire_.endTransmission();
+      AccelWire_.requestFrom(I2C_ADDR, uint8_t(3));
+      for (uint8_t i = 0; i < 3; i++)  // x, y, z
+      {
+        /* get register */
+        vector[i] = AccelWire_.read();
+        /* check the alert-bit */
+        if (vector[i] & (1 << 6)) alert = true;
+        else vector[i] = int8_t(vector[i] << 2) >> 2;   // calculate the signed 6-bit acceleration
+      }
+    } while (alert);  // repeat the whole crap if an alert bit is set!
+  }
 
-  AccelWire_.requestFrom(I2C_ADDR, uint8_t(1));
-  return AccelWire_.read();
-}
+/** -------------------------------------------------------------------------
+  * \fn     getAccelerationVector
+  * \brief  gets the acceleration vector (x, y, z)
+  *
+  * \param  vector  acceleration array pointer (float)
+  * \param  convert converts to a normed value if true
+  * \return None
+  --------------------------------------------------------------------------- */
+  void MMA7660FC::getAccelerationVector(float vector[3], bool convert)
+  {
+    int8_t tempVector[3];
+    getAccelerationVector(tempVector);
+    vector[0] = tempVector[0];
+    vector[1] = tempVector[1];
+    vector[2] = tempVector[2];
 
-void MMA7660FC::writeRegister(uint8_t address, uint8_t value)
-{
-  AccelWire_.beginTransmission(I2C_ADDR);
-  AccelWire_.write(address);
-  AccelWire_.write(value);
-  AccelWire_.endTransmission();
-}
+    if (convert)
+    {
+      vector[0] /= G_DIVISOR;
+      vector[1] /= G_DIVISOR;
+      vector[2] /= G_DIVISOR;
+    }
 
-/**
- * @}
- */
+    // bool alert;
+    // do
+    // {
+    //   alert = false;
+    //   AccelWire_.beginTransmission(I2C_ADDR);
+    //   AccelWire_.write(REG_X);
+    //   AccelWire_.endTransmission();
+    //   AccelWire_.requestFrom(I2C_ADDR, uint8_t(3));
+    //   for (uint8_t i = 0; i < 3; i++)  // x, y, z
+    //   {
+    //     /* get register */
+    //     vector[i] = AccelWire_.read();
+    //     /* check the alert-bit */
+    //     if (vector[i] & (1 << 6)) alert = true;
+    //     else vector[i] = int8_t(vector[i] << 2) >> 2;   // calculate the signed 6-bit acceleration
+
+    //     if (convert) vector[i] /= G_DIVISOR;
+    //   }
+    // } while (alert);  // repeat the whole crap if an alert bit is set!
+  }
+
+/** -------------------------------------------------------------------------
+  * \fn     getOrientation
+  * \brief  gets the orientation
+  *
+  * \return orientation (see enumerator in the header file)
+  --------------------------------------------------------------------------- */
+  MMA7660FC::orientation_t MMA7660FC::getOrientation()
+  {
+    orientation_t o = UNKNOWN;
+    int8_t vector[3] = {0};
+    getAccelerationVector(vector);
+    if      (vector[2] <= -ACCEL_THRESHOLD) o = DOWN;
+    else if (vector[2] >=  ACCEL_THRESHOLD) o = UP;
+    else if (vector[0] <= -ACCEL_THRESHOLD) o = HORIZONTALLY;
+    else if (vector[0] >=  ACCEL_THRESHOLD) o = HORIZONTALLY;
+    else if (vector[1] <= -ACCEL_THRESHOLD) o = VERTICALLY;
+    else if (vector[1] >=  ACCEL_THRESHOLD) o = VERTICALLY;
+    return o;
+  }
+
+/* ======================= Private member Functions ========================= */
+/** -------------------------------------------------------------------------
+  * \fn     readRegister
+  * \brief  reads the register of the given address
+  *
+  * \param  address  address of the accelerometer
+  * \return content of the given address
+  --------------------------------------------------------------------------- */
+  uint8_t MMA7660FC::readRegister(uint8_t address)
+  {
+    AccelWire_.beginTransmission(I2C_ADDR);
+    AccelWire_.write(address);
+    AccelWire_.endTransmission();
+
+    AccelWire_.requestFrom(I2C_ADDR, uint8_t(1));
+    return AccelWire_.read();
+  }
+
+/** -------------------------------------------------------------------------
+  * \fn     writeRegister
+  * \brief  writes the register of the given address
+  *
+  * \param  address  address of the accelerometer
+  * \param  value    value to write
+  * \return None
+  --------------------------------------------------------------------------- */
+  void MMA7660FC::writeRegister(uint8_t address, uint8_t value)
+  {
+    AccelWire_.beginTransmission(I2C_ADDR);
+    AccelWire_.write(address);
+    AccelWire_.write(value);
+    AccelWire_.endTransmission();
+  }
--- MMA7660FC.h
+++ MMA7660FC.h
@@ -1,44 +1,49 @@
-/**-----------------------------------------------------------------------------
- * \file    MMA7660FC.h
- * \author  jh
- * \date    xx.11.2016
- *
- * \version 1.0
- *
- * \brief   The MMA7660FC is a simple low power accelerometer.
- *
- * @{
- -----------------------------------------------------------------------------*/
+#ifndef _MMA7660FC_H_
+#define _MMA7660FC_H_
+/*******************************************************************************
+* \file    MMA7660FC.h
+********************************************************************************
+* \author  Jascha Haldemann jh@oxon.ch
+* \date    01.11.2016
+* \version 1.0
+*
+* \brief   The MMA7660FC is a simple low power accelerometer
+*
+********************************************************************************
+* Accelerometer Library
+*******************************************************************************/
 
-/* Define to prevent recursive inclusion -----------------------*/
-#ifndef MMA7660FC_H_
-#define MMA7660FC_H_
-
-/* Includes --------------------------------------------------- */
+/* ============================== Global imports ============================ */
 #include <Arduino.h>
 #include <Wire.h>
 
-/* Typedefs ----------------------------------------------------*/
-/* Macros ----------------------------------------------------- */
-/* Defines -----------------------------------------------------*/
+/* ==================== Global module constant declaration ================== */
 
-/* Class ------------------------------------------------------ */
+/* ========================= Global macro declaration ======================= */
+
+/* ============================ Class declaration =========================== */
 class MMA7660FC
 {
 public:
-	/* constructor(s) & deconstructor */
-	MMA7660FC(TwoWire& AccelWire) : AccelWire_(AccelWire) {};
-	~MMA7660FC() {};
-
-	/* public constants (static) */
-  // ...
-
-	/* public enumerations */
-	enum mode {STAND_BY = 0, ACTIVE = 1};  // Istand_by = 2... 10uA
-  enum orientation {UNKNOWN = 0, UP, DOWN, HORIZONTALLY, VERTICALLY};
-  enum sample_rate
+  /* Public member typedefs */
+  typedef enum : uint8_t
   {
-    AUTO_SLEEP_120 = 0X00,  // 120 sample per second
+    STAND_BY = 0,   // Istand_by = 2... 10uA
+    ACTIVE = 1
+  } mode_t;
+
+  typedef enum : uint8_t
+  {
+    UNKNOWN = 0,
+    UP = 1,
+    DOWN = 2,
+    HORIZONTALLY = 3,
+    VERTICALLY = 4
+  } orientation_t;
+
+  typedef enum : uint8_t
+  {
+    AUTO_SLEEP_120 = 0X00,  // 120 samples per second
     AUTO_SLEEP_64  = 0X01,
     AUTO_SLEEP_32  = 0X02,
     AUTO_SLEEP_16  = 0X03,
@@ -46,23 +51,24 @@ public:
     AUTO_SLEEP_4   = 0X05,
     AUTO_SLEEP_2   = 0X06,
     AUTO_SLEEP_1   = 0X07
-  };
+  } sampleRate_t;
 
-	/* public methods */
+  /* Constructor(s) and  Destructor */
+	MMA7660FC(TwoWire& AccelWire) : AccelWire_(AccelWire) {};
+	~MMA7660FC() {};
+
+	/* Public member functions */
   void begin(const uint32_t clockSpeed = 200000L);
   void end();
-  void setMode(mode modeNr);
-  void setSampleRate(sample_rate rateNr);
+  void setMode(mode_t modeNr);
+  void setSampleRate(sampleRate_t rateNr);
   void getAccelerationVector(int8_t vector[3]);
   void getAccelerationVector(float vector[3], bool convert = true);
-  orientation getOrientation();
+  orientation_t getOrientation();
   //TODO: implement a sleep()- or turnOff()-methode
 
 private:
-	/* attributes */
-	TwoWire& AccelWire_;
-
-  /* private constants (static) */
+  /* Private constant declerations (static) */
 	static const uint8_t I2C_ADDR        = 0x4C;
 
   static const uint8_t REG_X           = 0x00;
@@ -80,16 +86,12 @@ private:
   static const uint8_t G_DIVISOR       = 21;    // ~1g
   static const int8_t ACCEL_THRESHOLD  = 20;
 
-	/* private variables */
-  // ...
+	/* Private member data */
+  TwoWire& AccelWire_;
 
-	/* private methods */
+	/* Private member functions */
   uint8_t readRegister(uint8_t address);
   void writeRegister(uint8_t address, uint8_t value);
 };
 
 #endif
-
-/**
- * @}
- */
